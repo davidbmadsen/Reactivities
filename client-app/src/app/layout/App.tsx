0c3fe7665a51f80cc97163d0b9a221cb2b578df3
David Bjerregaard Madsen <davidm@netcompany.com>
2021-09-15 15:48:50 +0200

    Added UI on react app, TBC

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,28 @@
+import React, { Fragment, useEffect, useState } from 'react';
+import axios from 'axios';
+import { Container } from 'semantic-ui-react';
+import { Activity } from '../models/activity';
+import Navbar from './Navbar';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+
+function App() {
+
+  const [activities, setActivities] = useState<Activity[]>([]);
+
+  useEffect(() => {
+    axios.get<Activity[]>('http://localhost:5000/api/activities').then(response => {
+      setActivities(response.data);
+    })
+  }, [])
+
+  return (
+    <Fragment>
+      <Navbar />
+      <Container style={{ marginTop: '7em' }}>
+        <ActivityDashboard activities={activities}/>
+      </Container>
+    </Fragment>
+  );
+}
+
+export default App;
